7a8103aa0538f4d6b41500541d7f0394d53490ca
JANG <jsm_power@hanmail.net>
2016-05-19 09:35:53 +0800

    Project start

---
+++ atmel/Switcher/Switcher/main.c
@@ -0,0 +1,18 @@
+/*
+ * Switcher.c
+ *
+ * Created: 2016-05-19 오전 9:34:18
+ * Author : SOONMYUN
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
